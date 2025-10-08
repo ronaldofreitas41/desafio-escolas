fa7871d4d56062fc1c1806e8b463861c52b2b840
Ronaldo Luiz <65634807+ronaldofreitas41@users.noreply.github.com>
2025-10-08 13:55:23 -0300

    Consertando erros de import

--- Front/app/login/page.tsx
+++ Front/app/login/page.tsx
@@ -1,5 +1,6 @@
-import { LoginForm } from "@/components/login-form"
+
 import { GraduationCap, Shield, Zap, Database } from "lucide-react"
+import { LoginForm } from "../components/login-form"
 
 export default function LoginPage() {
   return (
