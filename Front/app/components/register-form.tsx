b6b3da84e29a6e2e998a1beab8219f6a111e4c4f
Ronaldo Luiz <65634807+ronaldofreitas41@users.noreply.github.com>
2025-10-08 17:54:51 -0300

    ajustes finais Login

--- Front/app/components/register-form.tsx
+++ Front/app/components/register-form.tsx
@@ -9,13 +9,18 @@ import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 import { Alert, AlertDescription } from "./ui/alert"
-import { Lock, Mail } from "lucide-react"
+import { IdCard, Lock, Mail, Phone } from "lucide-react"
 import { authAPI } from "@/lib/api"
+import InputMask from "react-input-mask";
 
-export function LoginForm() {
+export function RegisterForm() {
   const router = useRouter()
+  const [nome, setNome] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
+  const [CPF, setCPF] = useState("")
+  const [telefone, setTelefone] = useState("")
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
@@ -24,11 +29,38 @@ export function LoginForm() {
     redirect("/dashbooard");
   }
 
+  const formatCPF = (value: string) => {
+    return value
+      .replace(/\D/g, '') // Remove tudo que não é número
+      .replace(/(\d{3})(\d)/, '$1.$2') // Coloca ponto depois do terceiro dígito
+      .replace(/(\d{3})(\d)/, '$1.$2') // Coloca ponto depois do sexto dígito
+      .replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Coloca hífen depois do nono dígito
+  };
+
+  const formatTelefone = (value: string) => {
+    return value
+      .replace(/\D/g, '') // Remove tudo que não é número
+      .replace(/^(\d{2})(\d)/, '($1) $2') // Coloca parênteses nos dois primeiros dígitos
+      .replace(/(\d{5})(\d)/, '$1-$2') // Coloca hífen depois do quinto dígito
+      .slice(0, 15); // Limita o tamanho total
+  };
+
+  const handleCPFChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formatted = formatCPF(e.target.value);
+    setCPF(formatted);
+  };
+
+  const handleTelefoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formatted = formatTelefone(e.target.value);
+    setTelefone(formatted);
+  };
+
+
   return (
     <Card className="border-border">
       <CardHeader>
-        <CardTitle>Entrar</CardTitle>
-        <CardDescription>Digite suas credenciais para acessar o sistema</CardDescription>
+        <CardTitle>Registre-se</CardTitle>
+        <CardDescription>Informe seus dados para acessar o sistema</CardDescription>
       </CardHeader>
       <CardContent>
         <form onSubmit={handleSubmit} className="space-y-4">
@@ -37,6 +69,35 @@ export function LoginForm() {
               <AlertDescription>{error}</AlertDescription>
             </Alert>
           )}
+
+          <div className="space-y-2">
+            <Label htmlFor="nome">Nome</Label>
+            <div className="relative">
+              <Mail className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                id="nome"
+                type="nome"
+                placeholder="seu@nome.com"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                className="pl-10"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="relative">
+            <IdCard className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="text"
+              value={CPF}
+              onChange={handleCPFChange}
+              placeholder="000.000.000-00"
+              maxLength={14}
+              className="pl-10"
+              required
+            />
+          </div>
 
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
@@ -70,8 +131,41 @@ export function LoginForm() {
             </div>
           </div>
 
+          <div className="space-y-2">
+            <Label htmlFor="passwordConfirmation">Confirme sua Senha</Label>
+            <div className="relative">
+              <Lock className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                id="passwordConfirmation"
+                type="passwordConfirmation"
+                placeholder="••••••••"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                className="pl-10"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="Phone">Telefone</Label>
+            <div className="relative">
+              <Phone className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                id="telefone"
+                type="tel"
+                value={telefone}
+                onChange={handleTelefoneChange}
+                placeholder="(XX) XXXXX-XXXX"
+                className="pl-10"
+                required
+              />
+
+            </div>
+          </div>
+
           <Button type="submit" className="w-full" disabled={loading}>
-            {loading ? "Entrando..." : "Entrar"}
+            {loading ? "Salvando..." : "Registrar"}
           </Button>
         </form>
       </CardContent>
--- Front/app/register/page.tsx
+++ Front/app/register/page.tsx
@@ -1,19 +1,21 @@
-
+'use Client'
 import { GraduationCap, Shield, Zap, Database } from "lucide-react"
-import { LoginForm } from "../components/login-form"
+import { RegisterForm } from "../components/register-form"
 import Link from "next/link"
 
-export default function LoginPage() {
+
+
+export default function RegisterPage() {
     return (
         <div className="min-h-screen flex">
             <div className="hidden lg:flex lg:w-1/2 bg-card border-r border-border flex-col justify-between p-12">
                 <div>
                     <div className="flex items-center gap-3 mb-12">
                         <div className="p-2 bg-primary/10 rounded-lg">
-                            <GraduationCap className="h-8 w-8 text-primary" />
+                            <GraduationCap className="h-12 w-12 text-primary" />
                         </div>
                         <div>
-                            <h2 className="text-xl font-bold">EduSP Manager</h2>
+                            <h2 className="text-xl font-bold">Infra Manager</h2>
                             <p className="text-sm text-muted-foreground">Sistema de Gestão Escolar</p>
                         </div>
                     </div>
@@ -70,7 +72,7 @@ export default function LoginPage() {
             <div className="flex-1 flex items-center justify-center p-8">
                 <div className="w-full max-w-md space-y-2">
                     <div className="text-center space-y-2 lg:hidden">
-                        <div className="flex justify-center mb-4">
+                        <div className="flex justify-center mb-2">
                             <div className="p-3 bg-primary/10 rounded-lg">
                                 <GraduationCap className="h-10 w-10 text-primary" />
                             </div>
@@ -79,27 +81,32 @@ export default function LoginPage() {
                         <p className="text-muted-foreground">Sistema de Gestão Escolar</p>
                     </div>
 
-                    <div className="hidden lg:block space-y-2">
-                        <h2 className="text-2xl font-bold tracking-tight">Bem-vindo de volta</h2>
+                    <div className="hidden lg:block space-y-1">
+                        <h2 className="text-2xl font-bold tracking-tight">Bem-vindo!</h2>
                         <p className="text-muted-foreground">Entre com suas credenciais para acessar o sistema</p>
                     </div>
 
                     <div>
 
-                        <LoginForm />
+                        <RegisterForm />
                     </div>
+
                     <div className="text-center text-sm text-muted-foreground">
-                        Ainda não possui conta?{" "}
+                        Possui conta?{" "}
                         <Link
-                            className="text-primary hover:underline"
-                            href='/register'
+                        className="text-primary hover:underline"
+                        href = '/login'
                         >
-                            Registre-se
+                            Log In
                         </Link>
                     </div>
                     <div className="text-center text-sm text-muted-foreground">
-                        Problemas para acessar?{" "}
-                        <button className="text-primary hover:underline">Entre em contato com o suporte</button>
+                        Problemas para registrar?{" "}
+                        <button 
+                        className="text-primary hover:underline"
+                        >
+                            Entre em contato com o suporte
+                        </button>
                     </div>
                 </div>
             </div>
